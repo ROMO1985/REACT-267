bdf345a22e3006f2e96006a367f8ee407c83b489
ROMO1985 <romulo.coll@gmail.com>
2021-07-26 01:00:34 -0300

    Clase 03_Segunda Entrega

--- src/NavBar.js
+++ src/NavBar.js
@@ -4,8 +4,10 @@ import Button from 'react-bootstrap/Button'
 const NavBar = () =>
         
         <nav class="primary">
-
-                <a href="google">hola mundo</a>
+                <tr>
+                <a href="google">productos</a>
+                </tr>
+                <a href="google">Categorias</a>
                 
                 <Button variant="danger">Compra</Button> {' '}                
                 
